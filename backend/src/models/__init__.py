999bfe494eb15523b0725497bce0fa17757dd34d
wx528 <33819629+wx528@users.noreply.github.com>
2026-06-17 14:22:43 +0800

    refactor: update models __init__ to export only 6 models

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,39 +1,17 @@
 from src.core.database import Base
 from src.models.project import Project, ProjectStatus
-from src.models.notification import Notification, NotificationType
-from src.models.issue import Issue, IssueType, IssueStatus, IssuePriority, IssueSource
-from src.models.milestone import Milestone, MilestoneStatus
+from src.models.issue import Issue, IssueType, IssueStatus, IssuePriority
 from src.models.comment import Comment
-from src.models.plan import Plan, PlanStatus, PlanSource
+from src.models.plan import Plan, PlanStatus
 from src.models.plan_item import PlanItem, PlanItemStatus
-from src.models.activity_log import ActivityLog
-from src.models.server import Server, ServerType, ServerStatus, ServerEnvironment
-from src.models.workflow import (
-    Workflow, WorkflowStep, WorkflowRun, WorkflowStepRun,
-    WorkflowTrigger, WorkflowStatus, StepType, OnFailure, WorkflowRunStatus, StepRunStatus,
-)
-from src.models.agent_memory import AgentMemory
-from src.models.project_registration import ProjectRegistration, RegistrationStatus
-from src.models.feedback import Feedback, FeedbackCategory, FeedbackStatus
-from src.models.git_integration import GitIntegration, IssueCommitLink, PRPlanLink
-from src.models.risk_alert import RiskAlert, RiskAlertLevel, RiskAlertSource, RiskAlertStatus
+from src.models.notification import Notification
 
 __all__ = [
     "Base",
     "Project", "ProjectStatus",
-    "Notification", "NotificationType",
-    "Issue", "IssueType", "IssueStatus", "IssuePriority", "IssueSource",
-    "Milestone", "MilestoneStatus",
+    "Issue", "IssueType", "IssueStatus", "IssuePriority",
     "Comment",
-    "Plan", "PlanStatus", "PlanSource",
+    "Plan", "PlanStatus",
     "PlanItem", "PlanItemStatus",
-    "ActivityLog",
-    "Server", "ServerType", "ServerStatus", "ServerEnvironment",
-    "Workflow", "WorkflowStep", "WorkflowRun", "WorkflowStepRun",
-    "WorkflowTrigger", "WorkflowStatus", "StepType", "OnFailure", "WorkflowRunStatus", "StepRunStatus",
-    "AgentMemory",
-    "ProjectRegistration", "RegistrationStatus",
-    "Feedback", "FeedbackCategory", "FeedbackStatus",
-    "GitIntegration", "IssueCommitLink", "PRPlanLink",
-    "RiskAlert", "RiskAlertLevel", "RiskAlertSource", "RiskAlertStatus",
+    "Notification",
 ]

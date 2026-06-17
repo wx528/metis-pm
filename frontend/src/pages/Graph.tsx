dcb00cfd52e99d3c49a34f58d4fdd4ca03e51043
wx528 <33819629+wx528@users.noreply.github.com>
2026-06-17 14:58:16 +0800

    refactor: delete unused frontend pages, components, API modules, queries, hooks

--- frontend/src/pages/Graph.tsx
+++
@@ -1,9 +0,0 @@
-import GraphView from "../components/GraphView";
-
-export default function Graph() {
-  return (
-    <div style={{ height: "calc(100vh - 64px)" }}>
-      <GraphView />
-    </div>
-  );
-}
